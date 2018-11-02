1a74ad93002e4d14304d310b6ee1e2ca09bf6a80
ntthung <ntthung@gmail.com>
2018-11-02 16:13:55 +0800

    Add P1 annual streamflow data and documentation.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' P1 annual streamflow
+#'
+#' A dataset contaning annual streamflow of the Ping River measured at station P1 (Chiang Mai,
+#' Thailand).
+#'
+#' @format A data frame with 85 rows and 2 variables:
+#' \describe{
+#'   \item{year}{from 1921 to 1985}
+#'   \item{Qa}{annual streamflow in million cubic meters per month}
+#' }
+#' @source \url{https://www.hydro-1.net/08HYDRO/HD-04/4-06.html}
+"Ping.annual"
