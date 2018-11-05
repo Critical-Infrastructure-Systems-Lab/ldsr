3885bc86533c1d520c6fe91d29d55ac695b895d3
ntthung <ntthung@gmail.com>
2018-11-05 22:48:21 +0800

    Add test code.

--- experiments/test_package.R
+++ experiments/test_package.R
@@ -1,4 +1,3 @@
-# Setup -----------------------
 library(ldsr)
 # Check that package is self-contained so I shouldn't need to load data.table,
 # doParallel, ggplot2 and magrittr to run package functions
@@ -7,9 +6,28 @@ Qa <- Ping.annual
 u <- Ping.PC
 v <- Ping.PC
 
-fit <- LDS_reconstruction(Qa, u, v, num.restart = 2, parallel = TRUE)
+# Sequential learning
+# With init
+fit <- LDS_reconstruction(Qa, u, v, init = c(0.4, -0.03, 0.2, -0.03), parallel = FALSE)
+# Randomized
+fit <- LDS_reconstruction(Qa, u, v, num.restart = 5, parallel = FALSE)
+# Parallel learning
+# With init
+fit <- LDS_reconstruction(Qa, u, v, init = c(0.4, -0.03, 0.2, -0.03), parallel = TRUE)
+# Randomized
+fit <- LDS_reconstruction(Qa, u, v, num.restart = 5, parallel = TRUE)
 
-p <- plot_reconstruction(fit$rec, Qa, 'full')
-print(p)
-cowplot::ggsave('Test.pdf', p)
-cv <- cvLDS(Qa, u, v, init = c(0.4, -0.03, 0.2, -0.03), n.reps = 2)
+# Plot results
+plot_reconstruction(fit$rec, Qa, 'inst')
+plot_reconstruction(fit$rec, Qa, 'full')
+
+# Sequential CV
+# With init
+cv <- cvLDS(Qa, u, v, init = c(0.4, -0.03, 0.2, -0.03), n.reps = 5)
+# Ranomized restarts
+cv <- cvLDS(Qa, u, v, num.restart = 5, n.reps = 5)
+# Parallel CV
+# With init
+cv <- cvLDS(Qa, u, v, init = c(0.4, -0.03, 0.2, -0.03), n.reps = 5, parallel = TRUE)
+# Randomized
+cv <- cvLDS(Qa, u, v, num.restart = 8, n.reps = 5, parallel = TRUE)
