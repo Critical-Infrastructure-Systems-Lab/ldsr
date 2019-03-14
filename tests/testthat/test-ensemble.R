e17d0ed1b6991e611d13d06c740364a7c9516f5b
ntthung <ntthung@gmail.com>
2019-03-14 09:48:23 +0800

    Add unit test for ensemble model.

---
+++ tests/testthat/test-ensemble.R
@@ -0,0 +1,16 @@
+context("Ensemble model")
+
+u <- v <- t(P1pc[,c(1,3,4,6,9,11,12)])
+u.list <- list(u, u)
+v.list <- list(v, v)
+
+test_that("Ensemble model learning works", {
+    fit <- LDS_ensemble(P1annual, u.list, v.list, num.restarts = 12)
+    expect_is(fit, "list")
+})
+
+test_that("Ensemble cross validation works in serial mode", {
+    expect_is(cvLDS_ensemble(P1annual, u.list, v.list, CV.reps = 2, num.restarts = 2, parallel = FALSE),
+              "list")
+})
+
