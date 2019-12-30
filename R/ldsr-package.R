57f4905e84da84b95ff70725ccf9a50f6c485872
ntthung <ntthung@gmail.com>
2019-12-30 17:13:59 +0800

    Remove plotting dependencies.

--- R/ldsr-package.R
+++ R/ldsr-package.R
@@ -4,15 +4,13 @@
 #' @docType package
 #' @name ldsr
 #' @import data.table
-#' @import cowplot
-#' @rawNamespace import(ggplot2, except = ggsave)
+#' @import stats
 #' @importFrom doParallel registerDoParallel
 #' @importFrom foreach foreach %dopar% %:%
 #' @importFrom hydroGOF ssq NSE KGE rmse
 #' @importFrom magrittr %>%
 #' @importFrom parallel detectCores makeCluster stopCluster
 #' @importFrom Rcpp evalCpp sourceCpp
-#' @importFrom stats runif rnorm var sd optim lm predict step
 #' @useDynLib ldsr, .registration = TRUE
 #' @keywords internal
 "_PACKAGE"
