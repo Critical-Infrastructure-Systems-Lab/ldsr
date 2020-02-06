0e32bb9ad4d3d3baecee4435f984417d877df119
ntthung <ntthung@gmail.com>
2020-02-06 23:15:20 +0800

    Update PCR tests to the new API.

--- tests/testthat/test-benchmark.R
+++ tests/testthat/test-benchmark.R
@@ -2,15 +2,15 @@ context("Benchmark")
 
 test_that("Benchmark works", {
 
-    expect_is(PCR_reconstruction(P1annual, P1pc), "list")
+    expect_is(PCR_reconstruction(P1annual, P1pc, start.year = 1600), "list")
 })
 
 test_that("Benchmark works with AIC = -Infinity", {
 
-  expect_warning(fit <- PCR_reconstruction(ldsr:::P5annual, ldsr:::P5pc))
+  expect_warning(fit <- PCR_reconstruction(ldsr:::P5annual, ldsr:::P5pc, start.year = 1600))
   expect_is(fit, "list")
 })
 
 test_that("Benchmark ensemble works", {
-  expect_is(PCR_ensemble(P1annual, list(P1pc, P1pc)), "list")
+  expect_is(PCR_ensemble(P1annual, list(P1pc, P1pc), start.year = 1600), "list")
 })
