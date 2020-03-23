ca4902428ab8b32905d1279d1f2ee76cb2852f1e
ntthung <ntthung@gmail.com>
2020-03-23 23:42:56 +0800

    Update PCR test to reflect new API.

--- tests/testthat/test-PCR.R
+++ tests/testthat/test-PCR.R
@@ -1,15 +1,27 @@
-context("Benchmark")
+context("PCR")
 
-test_that("Benchmark works", {
-
-    expect_is(PCR_reconstruction(P1annual, P1pc[, c(1, 3, 4, 6, 9, 11, 12)], start.year = 1600), "list")
+test_that("PCR works", {
+  expect_is(PCR_reconstruction(P1annual, P1pc[, c(1, 3, 4, 6, 9, 11, 12)], start.year = 1600),
+            "list")
 })
 
-test_that("Benchmark cross-validation works", {
-
-  expect_is(cvPCR(P1annual, P1pc[, c(1, 3, 4, 6, 9, 11, 12)], start.year = 1600), "list")
+test_that("PCR cross-validation works", {
+  expect_is(cvPCR(P1annual, P1pc[, c(1, 3, 4, 6, 9, 11, 12)], start.year = 1600),
+            "list")
 })
 
-test_that("Benchmark ensemble works", {
-  expect_is(PCR_ensemble(P1annual, list(P1pc, P1pc), start.year = 1600), "list")
+test_that("PCR ensemble works", {
+  expect_is(PCR_reconstruction(P1annual,
+                               list(P1pc[, c(1, 3, 4, 6, 9, 11, 12)],
+                                    P1pc[, c(1, 3, 4, 6, 9, 11, 12)]),
+                               start.year = 1600),
+            "list")
+})
+
+test_that("PCR ensemble cross-validation works", {
+  expect_is(cvPCR(P1annual,
+                  list(P1pc[, c(1, 3, 4, 6, 9, 11, 12)],
+                       P1pc[, c(1, 3, 4, 6, 9, 11, 12)]),
+                  start.year = 1600),
+            "list")
 })
