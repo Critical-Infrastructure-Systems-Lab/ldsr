652fbb0dd57d5ffb04bf9cd15e56048172ea50a8
ntthung <ntthung@gmail.com>
2020-03-26 21:59:57 +0800

    Update tests according to new API.

--- tests/testthat/test-ensemble.R
+++ tests/testthat/test-ensemble.R
@@ -4,24 +4,14 @@ u <- v <- t(P1pc[,c(1,3,4,6,9,11,12)])
 u.list <- list(u, u)
 v.list <- list(v, v)
 
-test_that("Ensemble cross validation works in serial mode", {
-  expect_is(cvLDS_ensemble(P1annual, u.list, v.list, start.year = 1600, CV.reps = 2, num.restarts = 2, parallel = FALSE),
+test_that("Ensemble model works", {
+  expect_is(LDS_reconstruction(P1annual, u.list, v.list, start.year = 1600, num.restarts = 2),
             "list")
 })
 
-.runThisTest <- Sys.getenv("RunAllTests") == "yes"
 
-if (.runThisTest) {
-
-  test_that("Ensemble model learning works", {
-    fit <- LDS_ensemble(P1annual, u.list, v.list, start.year = 1600, num.restarts = 12)
-    expect_is(fit, "list")
-  })
-
-
-  test_that("Ensemble cross validation works in parallel", {
-    expect_is(cvLDS_ensemble(P1annual, u.list, v.list, start.year = 1600, CV.reps = 2, num.restarts = 2, parallel = TRUE),
-              "list")
-  })
-
-}
+test_that("Ensemble cross validation works", {
+  expect_is(cvLDS(P1annual, u.list, v.list, start.year = 1600, num.restarts = 2,
+                  Z = make_Z(P1annual$Qa, nRuns = 2, contiguous = FALSE)),
+            "list")
+})
--- tests/testthat/test-standard-calls.R
+++ tests/testthat/test-standard-calls.R
@@ -2,52 +2,23 @@ context("EM")
 
 u <- v <- t(P1pc[,c(1,3,4,6,9,11,12)])
 
-
 test_that("Fixed restarts works", {
   fit <- LDS_reconstruction(P1annual, u, v, start.year = 1600, method = 'EM',
-                            init = c(0.4, rep(-0.03, 7), 0.2, rep(-0.03, 7), 0.1, 0.1, 0, 0.1))
+                            init = make_init(nrow(u), 1))
   expect_is(fit, "list")
 })
 
-test_that("Randomized restarts works serially", {
-    expect_is(LDS_reconstruction(P1annual, u, v, start.year = 1600, method = 'EM',
-                                 num.restarts = 2,
-                                 parallel = FALSE),
+test_that("Randomized restarts works", {
+    expect_is(LDS_reconstruction(P1annual, u, v, start.year = 1600, method = 'EM', num.restarts = 2),
               "list")
 })
 
-test_that("Cross validation works in serial with fixed restarts", {
-    expect_is(cvLDS(P1annual, u, v, start.year = 1600, method = 'EM',
-                    init = c(0.4, rep(-0.03, 7), 0.2, rep(-0.03, 7), 0.1, 0.1, 0, 0.1),
-                    CV.reps = 2, parallel = FALSE),
+test_that("Cross validation works with fixed restarts", {
+    expect_is(cvLDS(P1annual, u, v, start.year = 1600, method = 'EM', init = make_init(nrow(u), 1)),
               "list")
 })
 
-test_that("Cross validation works in serial with randomized restarts", {
-    expect_is(cvLDS(P1annual, u, v, start.year = 1600, method = 'EM',
-                    num.restarts = 2,
-                    CV.reps = 2, parallel = FALSE),
+test_that("Cross validation works with randomized restarts", {
+    expect_is(cvLDS(P1annual, u, v, start.year = 1600, method = 'EM', num.restarts = 2),
               "list")
 })
-
-.runThisTest <- Sys.getenv("RunAllTests") == "yes"
-
-if (.runThisTest) {
-    test_that("Randomized restarts works in parallel with num.restarts > 10", {
-        expect_is(LDS_reconstruction(P1annual, u, v, start.year = 1600, method = 'EM',
-                                     num.restarts = 12,
-                                     parallel = TRUE),
-                  "list")
-    })
-    test_that("Cross validation works in parallel with randomized restarts", {
-      if (.runThisTest) {
-        expect_is(cvLDS(P1annual, u, v, start.year = 1600, method = 'EM',
-                        num.restarts = 2,
-                        CV.reps = 2, parallel = TRUE),
-                  "list")
-      }
-    })
-}
-
-
-
