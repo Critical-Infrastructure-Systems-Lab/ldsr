d9f20c7c779d65761ca16528135c30ec6957cd53
ntthung <ntthung@gmail.com>
2020-04-21 10:58:52 +0800

    Test everything for now.

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(ldsr)
 #
 # force tests to be executed if in dev release which we define as
 ## having a sub-release, eg 0.9.15.5 is one whereas 0.9.16 is not
-if (length(strsplit(packageDescription("ldsr")$Version, "\\.")[[1]]) > 3) {
-  Sys.setenv("RunAllTests" = "yes")
-}
+# if (length(strsplit(packageDescription("ldsr")$Version, "\\.")[[1]]) > 3) {
+#   Sys.setenv("RunAllTests" = "yes")
+# }
 test_check("ldsr")
--- tests/testthat/test-non-standard-calls.R
+++ tests/testthat/test-non-standard-calls.R
@@ -2,10 +2,6 @@ context('non-standard EM')
 
 u <- v <- t(NPpc[601:813])
 
-.runThisTest <- Sys.getenv("RunAllTests") == "yes"
-
-if (.runThisTest) {
-  test_that('Return-raw = TRUE works', {
+test_that('Return-raw = TRUE works', {
     expect_is(LDS_reconstruction(NPannual, u, v, start.year = 1800, num.restarts = 2, return.raw = TRUE), "list")
-  })
-}
+})
